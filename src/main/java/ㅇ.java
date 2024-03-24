21a9dd5be2cadc56e96a227bff1eb56782d0112e
tt2t2am1118 <tt2t2am1118@naver.com>
2024-03-24 09:24:07 +0900

    first commit

---
+++ src/main/java/ㅇ.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import javax.swing.*;
+/*
+ * Created by JFormDesigner on Fri Mar 22 07:38:20 KST 2024
+ */
+
+
+
+/**
+ * @author ee323
+ */
+public class ㅇ extends JFrame {
+    public ㅇ() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Hyeonil
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new FlowLayout());
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Hyeonil
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
