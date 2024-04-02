ade488fc74a71884a117b8db72407634c5419c5e
tt2t2am1118 <tt2t2am1118@naver.com>
2024-04-02 09:00:20 +0900

    How about this~. Let's study, programming. Funny day. Wiki quiz end. Try this file~. Thank you.

--- src/main/java/TestWordCheck.java
+++ src/main/java/TestWordCheck.java
@@ -1,8 +1,10 @@
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 //How about this~. Let's make~. Yours~. Funny day~. Thank you.
+// https://github.com/infott2t/JavaSwingEX/wiki  wiki quiz~.
 public class TestWordCheck {
 
     String[] KeysInputStrs;
@@ -17,67 +19,7 @@ public class TestWordCheck {
         this.TablesAllStrs = new String[]{"user_info", "user", "shop_info", "service1Name", "service2Name", "service3Name"}; // input words. Table.
 
 
-        this.tablesMap = new Map<String, String[]>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean containsKey(Object key) {
-                return false;
-            }
-
-            @Override
-            public boolean containsValue(Object value) {
-                return false;
-            }
-
-            @Override
-            public String[] get(Object key) {
-                return new String[0];
-            }
-
-            @Override
-            public String[] put(String key, String[] value) {
-                return new String[0];
-            }
-
-            @Override
-            public String[] remove(Object key) {
-                return new String[0];
-            }
-
-            @Override
-            public void putAll(Map<? extends String, ? extends String[]> m) {
-
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Set<String> keySet() {
-                return null;
-            }
-
-            @Override
-            public Collection<String[]> values() {
-                return null;
-            }
-
-            @Override
-            public Set<Entry<String, String[]>> entrySet() {
-                return null;
-            }
-        };  // Map init, collet or not?. Need your help or not.
+        this.tablesMap = new HashMap<>();  // Map init, collet or not?. Need your help or not.
 
         tablesMap.put("ADMIN", new String[]{"user_info", "user"});
         tablesMap.put("SHOPPER", new String[]{"shop_info"});
@@ -85,9 +27,8 @@ public class TestWordCheck {
 
         if("ADMIN".equals(key)) {
             resultUsingTables = tablesMap.get("ADMIN");
-            for(String table : resultUsingTables) {
-
-                System.out.println(table);  // Good print result?. Need your help or not.
+            for(int i=0 ; i< resultUsingTables.length ; i++) {
+                System.out.println(resultUsingTables[i]);
             }
 
         } else if("SHOPPER".equals(key)) {
